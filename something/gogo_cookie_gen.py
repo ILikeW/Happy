827f2825b73ac864181bd9cf8542e2d6337617a8
dannny <64869749+ILikeW@users.noreply.github.com>
2024-09-10 09:32:24 -0400

    Update gogo_cookie_gen.py

--- something/gogo_cookie_gen.py
+++ something/gogo_cookie_gen.py
@@ -1,4 +1,5 @@
 import requests
+import os
 from bs4 import BeautifulSoup as bs
 
 
@@ -29,5 +30,16 @@ def get_gogo_cookie(email, password):
         return s.cookies.get_dict().get("auth")
 
 
-with open("gogoCookie.txt", "w") as f:
-    f.write(get_gogo_cookie("qwertytechz123@gmail.com", "P@8eqB7@YpJz5ea"))
+# Get email and password from environment variables
+email = os.environ.get('GOGO_EMAIL')
+password = os.environ.get('GOGO_PASSWORD')
+
+if email and password:
+    with open("gogoCookie.txt", "w") as f:
+        cookie = get_gogo_cookie(email, password)
+        if cookie:
+            f.write(cookie)
+        else:
+            print("Failed to generate cookie")
+else:
+    print("Email or password not provided in environment variables")
